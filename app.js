758b317d39174e0252080a1ae52220c9cc468c8b
SantG25 <102637554+SantG25@users.noreply.github.com>
2026-07-06 14:11:54 -0400

    Show error on input

--- app.js
+++ app.js
@@ -5,14 +5,27 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:false}));
+const {check, validationResult} = require('express-validator'); // ES6 standard for destructuring an object
 
 app.get('/', (req, res) => {
 res.render('form');
 });
 
 //To handle the form info
-app.post('/process',(req, res) => {
+app.post('/process',[
+  check('name', 'Must have a name').notEmpty(),
+  check('email', 'Must have email').isEmail()
+  ],
+(req, res) => {
+  
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+      //When there are errors in form
+      res.render('form', {errorInfo: errors.array()} );
 
+  }else{
+
+      //When there's no errors
     var name = req.body.name;
     var email = req.body.email;
     var postcode = req.body.postcode;
@@ -21,10 +34,11 @@ app.post('/process',(req, res) => {
     var numTickets = req.body.tickets;
     var campus = req.body.campus;
 
-    console.log(`${needLunch}' --${numTickets} -- ${campus}`);
+    console.log(`${needLunch} --${numTickets} -- ${campus}`);
 
     res.redirect('/');
 
+  }
 });
 
 
