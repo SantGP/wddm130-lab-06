84fd3a60bede40f766b0fe31af87a7c81b47cb8d
SantG25 <102637554+SantG25@users.noreply.github.com>
2026-07-13 13:28:54 -0400

    Final version

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.post('/process',[
   check('phone',"Invalid phone number").matches(/^\d{3}(\s|-)\d{3}(\s|-)\d{4}$/),
 
   check('lunch').custom(async (value, {req})=>{
-    if (value == "yes" && req-body.tickets < 2){
+    if (value == "yes" && req.body.tickets < 2){
       throw new Error('If lunch selected, need at least 2 tickets');
     }
   })
@@ -45,9 +45,22 @@ app.post('/process',[
     var numTickets = req.body.tickets;
     var campus = req.body.campus;
 
-    console.log(`${needLunch} --${numTickets} -- ${campus}`);
+    var lunchCost = needLunch =="yes" ? 60:0;
+    var ticketCost = numTickets * 100;
+    var sub = lunchCost + ticketCost;
+    
+    var recpt={
+      'name' : name,
+      'lunchCost' : lunchCost,
+      'ticketCost' : ticketCost,
+      'subTotal' : sub,
+      'taxes' : 0.13*sub,
+      'total' : 1.13*sub
+    }
+    res.render ('form',{receipt: recpt})
 
-    res.redirect('/');
+    //console.log(`${needLunch} --${numTickets} -- ${campus}`);
+    //res.redirect('/');
 
   }
 });
